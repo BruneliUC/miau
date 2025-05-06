81418029e76161241d830978ef560b39b55d9ca8
BruneliUC <br1bierwirth@estudiante.uc.cl>
2025-05-05 22:43:48 -0400

    se modifica el readme, se añade curiosidades.py finalizado y se añade informacion_confidencial.txt sin ser confidencial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # miau
+
+| Nombre                   |  Github       | Email              |
+| ------------------------ |  ------------ | ------------------ |
+| Bruno Bierwirth           |   @BruneliUC | br1bierwirth@estudiante.uc.cl   |
+
+## Aportes de cada uno:
+> Bruno: Hizo todo
---
+++ curiosidades.py
@@ -0,0 +1,31 @@
+import random
+a=random.randint(1,10)
+if a==1:
+    print("Las jirafas tienen 30 veces más probabilidades de ser alcanzadas por un rayo que las personas. Es cierto que sólo hay cinco rayos mortales bien documentados sobre jirafas entre 1996 y 2010. Pero debido a que la población de la especie es de sólo 140.000 durante este tiempo, esto supone alrededor de 0,003 muertes por rayo por cada mil jirafas cada año. Esto es 30 veces la tasa de mortalidad equivalente para los seres humanos.")
+
+elif a==2:
+    print("El agua puede no ser húmeda. Esto se debe a que la mayoría de los científicos definen la humedad como la capacidad de un líquido de mantener el contacto con una superficie sólida, lo que significa que el agua en sí no es húmeda, pero puede hacer que otros objetos lo sean.")
+
+elif a==3:
+    print("Llevar corbata puede reducir el flujo sanguíneo al cerebro en un 7,5 Un estudio realizado en 2018 descubrió que llevar corbata puede reducir el flujo sanguíneo al cerebro hasta un 7,5%, lo que puede hacerte sentir mareado, con náuseas y provocar dolores de cabeza. También pueden aumentar la presión en los ojos si se llevan demasiado apretadas y son excelentes portadoras de gérmenes.")
+
+elif a==4:
+    print("El miedo a las palabras largas se denomina Hipopotomonstrosesquippedaliofobia. Esta palabra de 36 letras fue utilizada por primera vez por el poeta romano Horacio en el siglo I a.C. para criticar a los escritores con una afición irracional por las palabras largas. Fue la poeta estadounidense Aimee Nezheukumatathil, posiblemente asustada de su propio apellido, quien acuñó el término tal y como lo conocemos en 2000.")
+
+elif a==5:
+    print("Uno puede morirse de risa. Varias personas lo han hecho, normalmente debido a una risa intensa que les provocó un ataque al corazón o asfixia. Los espectáculos de humor deberían ir acompañados de una advertencia.")
+
+elif a==6:
+    print("Los plátanos son radiactivos. Al ser ricos en potasio, todos los plátanos son ligeramente radiactivos, ya que contienen el isótopo natural potasio-40. Es interesante saber que el organismo contiene alrededor de 16.000 toneladas de potasio al año. Curiosamente, tu cuerpo contiene alrededor de 16 mg de potasio-40, lo que significa que ya eres unas 280 veces más radiactivo que un plátano. Cualquier exceso de potasio-40 que obtengas de un plátano se excreta en pocas horas.")
+
+elif a==7:
+    print("Escocia eligió el unicornio como animal nacional. En la mitología celta, esta criatura de ficción está relacionada tanto con la caballerosidad y el dominio como con la pureza y la inocencia.")
+
+elif a==8:
+    print("La berenjena contiene nicotina. Pero tendrías que comer 9 kilos para obtener el mismo efecto que con un cigarro.")
+
+elif a==9:
+    print("Las aves no orinan. Ni siquiera tienen uretra... todo sale de la cola.")
+
+elif a==10:
+    print("Los delfines se ponen nombre. Los delfines tienen silbidos únicos, y los otros delfines imitan ese silbido para llamarles la atención. Los investigadores de delfines piensan que este silbido es su versión de tener nombre.")
